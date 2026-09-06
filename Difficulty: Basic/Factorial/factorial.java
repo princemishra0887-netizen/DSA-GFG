978d268292c54a9b7e8258dd78030a6690fbc50a
Prince Mishra <princemishra0887@gmail.com>
2026-09-06 22:50:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,10 +1,11 @@
 class Solution {
     int factorial(int n) {
         
-        int fact =1;
+         int fact =1;
         for(int i=1; i<=n; i++){
             fact=fact*i;
         }
         return fact;
+      
     }
 }
